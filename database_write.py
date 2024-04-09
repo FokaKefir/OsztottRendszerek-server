afc40eef8e5ef81a7e82f312009fd6bfa86e9f38
Babos Dávid <babosdavid8@gmail.com>
2024-04-09 18:09:48 +0300

    Add 4 write and update method for firestore

--- database_write.py
+++ database_write.py
@@ -5,15 +5,59 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate('forms_key.json')
 firebase_admin.initialize_app(cred)
 
-# get firestore
-
+# firestore db
 db = firestore.client()
 
-def register_user(email: str, name: str, password: str, phone: str, role: str = "user"):
-    pass
+# referencies
+users_ref = db.collection('users')
+submits_ref = db.collection('submits')
+forms_ref = db.collection('forms')
 
 
-if __name__ == '__main__':
-    user_ref = db.collection('users').document('user1')
-    docSnapshot = user_ref.get()
-    print(docSnapshot.to_dict())
+# register user then return user_id
+def register_user(user: dict) -> str:
+    # set default role to user
+    if 'role' not in user.keys():
+        user['role'] = 'user'
+
+    # add user to users collection
+    _, new_user_ref = users_ref.add(user)
+
+    # return new user's uid
+    return new_user_ref.id
+    
+# create a new submission then return its id
+def create_submission(form_id: str, user_id: str, options: list) -> str:
+    # create document dictionary
+    submit = {
+        'user_id': user_id,
+        'form_id': form_id,
+        'options': options
+    }
+
+    # add submit to the collection
+    _, new_submit_ref = submits_ref.add(submit)
+
+    # return new submit's uid
+    return new_submit_ref.id
+
+# create a new form then return 
+def create_form(form: dict) -> str:
+    # set default active value for the form
+    if 'active' not in form.keys():
+        form['active'] = True
+
+    # add form to the forms collection
+    _, new_form_ref = forms_ref.add(form)
+
+    # return new form's uid
+    return new_form_ref.id
+
+# change the status of the form by it's id
+def change_form_status(form_id: str, status: bool) -> None:
+    # get form reference by form_id
+    form_ref = forms_ref.document(form_id)
+
+    # update document
+    form_ref.update({'active': status})
+    return
